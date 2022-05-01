688b3312a2ff27423dfc2201d1206a4a3d711df0
baluart <lucasbaluart@gmail.com>
2022-04-30 23:07:20 -0300

    resositories criados

--- src/main/java/com/lucas/helpdesk/HelpDeskApplication.java
+++ src/main/java/com/lucas/helpdesk/HelpDeskApplication.java
@@ -1,13 +1,51 @@
 package com.lucas.helpdesk;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lucas.helpdesk.domain.Chamado;
+import com.lucas.helpdesk.domain.Cliente;
+import com.lucas.helpdesk.domain.Tecnico;
+import com.lucas.helpdesk.domain.enums.Perfil;
+import com.lucas.helpdesk.domain.enums.Prioridade;
+import com.lucas.helpdesk.domain.enums.Status;
+import com.lucas.helpdesk.repositories.ChamadoRepository;
+import com.lucas.helpdesk.repositories.ClienteRepository;
+import com.lucas.helpdesk.repositories.TecnicoRepository;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 @SpringBootApplication
-public class HelpDeskApplication {
+public class HelpDeskApplication implements CommandLineRunner {
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpDeskApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Tecnico tec1 = new Tecnico(null,"Lucas Nunes","01915254213","lucasbaluart@gmail.com","123");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cd_cli1 = new Cliente(null,"Ester Lima","04994011230","ester.lima.ferrera@gmail.com","123");
+		
+		Chamado chm1 = new Chamado(null, Prioridade.MEDIA,Status.ANDAMENTO,"Chamado 01","primeiro chamado",tec1,cd_cli1);
+		
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cd_cli1));
+		chamadoRepository.saveAll(Arrays.asList(chm1));
 	}
 
 }
